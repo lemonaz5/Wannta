e9d53803b1dbafd7e2db201f110f578e3b91da68
Unknown <burin010n@gmail.com>
2018-04-28 02:34:48 +0700

    fix bug

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ EOS_ID = 3
 TESTSET_SIZE = 25000
 
 BUCKETS = [(19, 19), (28, 28), (33, 33), (40, 43), (50, 53), (60, 63)]
-
+#BUCKETS = [(19, 19), (28, 28)]
 
 CONTRACTIONS = [("i ' m ", "i 'm "), ("' d ", "'d "), ("' s ", "'s "),
 				("don ' t ", "do n't "), ("didn ' t ", "did n't "), ("doesn ' t ", "does n't "),
@@ -42,11 +42,11 @@ CONTRACTIONS = [("i ' m ", "i 'm "), ("' d ", "'d "), ("' s ", "'s "),
 
 NUM_LAYERS = 3
 HIDDEN_SIZE = 256
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
 LR = 0.5
 MAX_GRAD_NORM = 5.0
 
 NUM_SAMPLES = 512
-ENC_VOCAB = 24346
-DEC_VOCAB = 24626
+ENC_VOCAB = 1046
+DEC_VOCAB = 1046
